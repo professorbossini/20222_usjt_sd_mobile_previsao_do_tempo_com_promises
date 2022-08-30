4ec66f1205701bca76e4fc7beed220cfd52f1f33
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-29 21:04:19 -0300

    apostila02: bloco de codigo 3.3.4
    dependencias
    dotenv
    url montada e testada

---
+++ index.js
@@ -0,0 +1,20 @@
+//dotenv
+require('dotenv').config()
+const axios = require ('axios')
+const appid = process.env.appid
+
+const q = 'Itu'
+
+//metric: celsius
+//imperial: farenheit
+const units = 'metric'
+
+const lang = 'pt_BR'
+
+const cnt = 10
+
+const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
+
+const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
+
+console.log(url)
