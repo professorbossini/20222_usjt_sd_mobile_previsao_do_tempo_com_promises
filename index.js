8a6e64fe2a4ef3ab9de7193f5d588c5769da5b5c
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-29 21:26:12 -0300

    apostila02: primeiro get realizado

--- index.js
+++ index.js
@@ -16,5 +16,10 @@ const cnt = 10
 const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
 const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
+const p = axios.get(url)
+p
+.then((res) => {
+  console.log(res.data)
+  return res.data
+})
 
-console.log(url)
